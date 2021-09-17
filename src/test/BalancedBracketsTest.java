0444210ab3fba7e3f6f81be3bdfbb3fad9e42b18
andreachastain <andreachancey@gmail.com>
2021-09-16 20:15:42 -0500

    finished

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,8 +1,11 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
+
 
 public class BalancedBracketsTest {
 
@@ -13,4 +16,64 @@ public class BalancedBracketsTest {
     }
 
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue2() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue3() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue4() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue5() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue6() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalse6() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalse5() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalse4() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalse3() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalse1() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[word]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsFalse2() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[word]]"));
+    }
 }
